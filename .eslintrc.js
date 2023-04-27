20e358ad7c30c33300c205dd947d74514977b7da
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-27 19:34:30 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always',
+    ],
+    'arrow-spacing': [
+      'error', { before: true, after: true },
+    ],
+    'no-console': 0,
+  },
+};
